efed39b5cca41a07dd360c20fc2d9b57104705f6
Chris <1995cbald@gmail.com>
2022-11-02 17:52:14 -0400

    fixed up random computer select

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
 
 
+let guessSubmit = document.querySelector('.guessSubmit');
+let guessField = document.querySelector('.guessField');
 
-
+let arr = ["Rock", "Paper", "Scissors"]; 
+let randomAction = Math.floor(Math.random() * arr.length);
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
+    if UserPick == "rock" && getComputerChoice == "scissors";
+    return "You Win Mista White!"
   }
 
-  
-function getComputerChoice() {
-    //inside works so maybe cut from function?
-let arr = ["Rock", "Paper", "Scissors"];
-let randomAction = Math.floor(Math.random() * arr.length);
+
+//function getComputerChoice() {
+    //inside works so maybe cut from function? let arr = ["Rock", "Paper", "Scissors"]; let randomAction = Math.floor(Math.random() * arr.length);
 }
 
 
-
-
-
-
-  
-
-
-
-function battle() {
-
-let userPick = String(weaponField.value)
-    if (userPick == "rock") {
-
-
-    }
