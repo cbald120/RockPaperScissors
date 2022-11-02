17ef77f0777dd356a3d237373a3af4a57053b407
Chris <1995cbald@gmail.com>
2022-11-02 15:21:02 -0400

    created random selector for computer action

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+
+  
+function getComputerChoice() {
+    //inside works so maybe cut from function?
+let arr = ["Rock", "Paper", "Scissors"];
+let randomAction = Math.floor(Math.random() * arr.length);
+}
+
+
+
+
+
+
+  
+
+
+
+function battle() {
+
+let userPick = String(weaponField.value)
+    if (userPick == "rock") {
+
+
+    }
