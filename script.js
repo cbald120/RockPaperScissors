0a9adf34138084d7335e7604242d12d3133a1001
Chris <1995cbald@gmail.com>
2022-11-03 09:44:33 -0400

    Fiddled with the round loop

--- script.js
+++ script.js
@@ -16,43 +16,53 @@ greet();
 //function computerSelection(weapon) {
 
 
+
+
+//current objectives: create rounds, create answer for incorrect input (jesse), create score tab in HTML, 
+
 function getComputerChoice() {
     let getChoice = ["Gus", "Hector", "Walter"]; 
     return getChoice[Math.floor(Math.random() * getChoice.length)];
     
 }
 
-let playerSelection = prompt("Choose Your Character");
-let computerSelection = getComputerChoice();
+
+let playerSelection = prompt("Choose your character").toUpperCase(0);
+let computerSelection = getComputerChoice().toUpperCase(0);
+
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "Gus" && computerSelection == "Walter") {
-        return ("If you try to interfere, this becomes a much simpler matter. You lose");} 
-    else if (playerSelection == "Hector" && computerSelection == "Gus") {
+    if (playerSelection == "GUS" && computerSelection == "WALTER") {
+        return ("If you try to interfere, this becomes a much simpler matter. You win");} 
+    else if (playerSelection == "HECTOR" && computerSelection == "GUS") {
         return ("Ding ding ding. You WIN");}  
-    else if (playerSelection == "Walter" && computerSelection == "Hector"){
+    else if (playerSelection == "WALTER" && computerSelection == "HECTOR"){
         return ("Ya win Mista White!");}  
     else if (playerSelection === computerSelection) {
        return ("A tie...This is what comes from blood for blood");} 
-    else if (playerSelection == "Gus" && computerSelection == "Hector") {
+    else if (playerSelection == "GUS" && computerSelection == "HECTOR") {
         return ("Ding ding ding. You lose") }
-    else if (playerSelection == "Hector" && computerSelection == "Walter") {
+    else if (playerSelection == "HECTOR" && computerSelection == "WALTER") {
         return ("I am the danger. You lose.") }
-    else if (playerSelection == "Walter" && computerSelection == "Gus") {
+    else if (playerSelection == "WALTER" && computerSelection == "GUS") {
         return ("Never do the same mistake twice, Walter")}
+    
+
    }
 
+//should i define i by wins or rounds? Which is easier?
 
+   
 
    function game() {
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Choose Your Character");
-        let computerSelection = getComputerChoice();
+        let i = +"win";
+        console.log(playRound(playerSelection, computerSelection));
+
      }
-     function getComputerChoice() {
-        let getChoice = ["Gus", "Hector", "Walter"]; 
-        return getChoice[Math.floor(Math.random() * getChoice.length)];
-        
-    }
-     playRound(playerSelection, computerSelection);
+ 
    }
+
+   //let playerSelection = window.prompt("Enter your choice: ").toLowerCase();
+
+ playRound(playerSelection, computerSelection);
