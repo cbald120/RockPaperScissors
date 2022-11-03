9825cef874342b1712d86031c0a882547f92df09
Chris <1995cbald@gmail.com>
2022-11-03 07:57:30 -0400

    Finalized the playround function and fixed player input Changed theme around Breaking Bad for fun Need to consider how to communicate what order wins in this tweaked game going to work on connecting the JS to the HTML input soon

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 
-
-let guessSubmit = document.querySelector('.guessSubmit');
-let guessField = document.querySelector('.guessField');
-
-let arr = ["Rock", "Paper", "Scissors"]; 
-let randomAction = Math.floor(Math.random() * arr.length);
-
-function playRound(playerSelection, computerSelection) {
-    if UserPick == "rock" && getComputerChoice == "scissors";
-    return "You Win Mista White!"
-  }
-
-
-//function getComputerChoice() {
-    //inside works so maybe cut from function? let arr = ["Rock", "Paper", "Scissors"]; let randomAction = Math.floor(Math.random() * arr.length);
+//Example of proper function
+function greet () {
+    console.log("Hello World"); //you can add a parameter with + '' to make it say Hello Chris
 }
 
+greet();
 
+
+
+//let guessSubmit = document.querySelector('.guessSubmit');
+//let guessField = document.querySelector('.guessField');
+//let result = document.querySelector('.result');
+//const lastResult = document.querySelector('.lastResult');
+
+//function computerSelection(weapon) {
+
+
+function getComputerChoice() {
+    let getChoice = ["Gus", "Hector", "Walter"]; 
+    return getChoice[Math.floor(Math.random() * getChoice.length)];
+    
+}
+
+//let playerSelection = "Rock";
+let playerSelection = prompt("Choose Your Character");
+let computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Gus" && computerSelection == "Walter") {
+        return ("If you try to interfere, this becomes a much simpler matter. You lose");} 
+    else if (playerSelection == "Hector" && computerSelection == "Gus") {
+        return ("Ding ding ding. You WIN");}  
+    else if (playerSelection == "Walter" && computerSelection == "Hector"){
+        return ("Ya win Mista White!");}  
+    else if (playerSelection === computerSelection) {
+       return ("A tie...This is what comes from blood for blood");} 
+    else if (playerSelection == "Gus" && computerSelection == "Hector") {
+        return ("Ding ding ding. You lose") }
+    else if (playerSelection == "Hector" && computerSelection == "Walter") {
+        return ("I am the danger. You lose.") }
+    else if (playerSelection == "Walter" && computerSelection == "Gus") {
+        return ("Never do the same mistake twice, Walter")}
+   }
